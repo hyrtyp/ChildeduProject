8ecbd97e4b906424fbed7b5ced3cdbd7f09d6ece
GYH <790005156>
2013-10-14 11:04:27 +0800

    Merge remote-tracking branch 'origin/master'

--- Childedu/src/main/java/com/hyrt/childedu/BaseActivity.java
+++ Childedu/src/main/java/com/hyrt/childedu/BaseActivity.java
@@ -12,7 +12,7 @@ import com.octo.android.robospice.spicelist.BitmapSpiceManager;
 import roboguice.activity.RoboActivity;
 
 /**
- * global base activity yepeng4
+ * global base activity yepeng6
  */
 public class BaseActivity extends RoboSherlockActivity{
 
