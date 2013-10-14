410cdf1920a30411107220289cb6e93b29df1897
GYH <790005156>
2013-10-14 10:11:19 +0800

    add describe

--- Childedu/src/main/java/com/hyrt/childedu/BaseActivity.java
+++ Childedu/src/main/java/com/hyrt/childedu/BaseActivity.java
@@ -12,7 +12,7 @@ import com.octo.android.robospice.spicelist.BitmapSpiceManager;
 import roboguice.activity.RoboActivity;
 
 /**
- * global base activity
+ * global base activity xiaohuoban
  */
 public class BaseActivity extends RoboSherlockActivity{
 
