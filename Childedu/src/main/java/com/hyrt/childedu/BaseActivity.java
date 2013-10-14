12ea566e9c1d6521b04ff0591217d6bd5d6b499f
hyrtyp <13718868826@163.com>
2013-10-14 10:36:13 +0800

    add describe

--- Childedu/src/main/java/com/hyrt/childedu/BaseActivity.java
+++ Childedu/src/main/java/com/hyrt/childedu/BaseActivity.java
@@ -6,7 +6,7 @@ import com.octo.android.robospice.SpiceManager;
 import com.octo.android.robospice.spicelist.BitmapSpiceManager;
 
 /**
- * global base activity yepeng xiaohuoban
+ * global base activity xiaohuoban
  */
 public class BaseActivity extends RoboSherlockActivity{
 
